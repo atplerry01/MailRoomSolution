7537e398b01b7d1608a1db9739992a2a0d8d027f
Akinsanya Olanrewaju <atplerry@gmail.com>
2018-05-28 16:39:44 +0100

    EmailPDF Completed

--- MailRoom.Api/Controllers/SendEmailController.cs
+++ MailRoom.Api/Controllers/SendEmailController.cs
@@ -1,29 +1,66 @@
+using System;
+using System.IO;
 using System.Net;
 using System.Net.Mail;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using MimeKit;
 
 namespace MailRoom.Api.Controllers
 {
     [Route("api/sendmail")]
-    public class SendEmailController: Controller
+    public class SendEmailController : Controller
     {
-        public SendEmailController()
+        private readonly IHostingEnvironment host;
+        public SendEmailController(IHostingEnvironment host)
         {
-            
+            this.host = host;
         }
 
         [HttpGet]
         public async Task<IActionResult> GetJobManifest()
         {
-            string text= "<table><tr><td>EmpId</td><td>Emp name</td><td>age</td></tr><tr><td>value</td><td>value</td><td>value</td></tr></table>";
+
+            var uploadFolderPath = Path.Combine(host.WebRootPath, "templates/email-template");
+            if (Directory.Exists(uploadFolderPath))
+                Directory.CreateDirectory(uploadFolderPath);
+
+            var fileName = "Confirm_Account_Registration.html"; //Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
+            var filePath = Path.Combine(uploadFolderPath, fileName);
+
+            var builder = new BodyBuilder();
+
+            using (StreamReader SourceReader = System.IO.File.OpenText(filePath))
+            {
+                builder.HtmlBody = SourceReader.ReadToEnd();
+            }
+
+            var subject = "Subject";
+            var Email = "atplerry@gmail.com";
+            var Password = "olami";
+            var Message = "Messaage End";
+            var callbackUrl = "http://google.com";
+
+            string messageBody = string.Format(builder.HtmlBody,
+                        subject,
+                        String.Format("{0:dddd, d MMMM yyyy}", DateTime.Now),
+                        Email,
+                        Email,
+                        Password,
+                        Message,
+                        callbackUrl
+                        );
+
+            /////////////////////////
+            string text = "<table><tr><td>EmpId</td><td>Emp name</td><td>age</td></tr><tr><td>value</td><td>value</td><td>value</td></tr></table>";
 
             var mailMessage = new MailMessage
             {
                 From = new MailAddress("lakinsanya@secureidltd.com"),
                 Subject = "Hello World",
                 IsBodyHtml = true,
-                Body = text //"Test email from Send Grid SMTP Settings"
+                Body = messageBody //text //"Test email from Send Grid SMTP Settings"
             };
 
             mailMessage.To.Add("atplerry@gmail.com, lakinsanya@secureidltd.com");
@@ -39,6 +76,6 @@ namespace MailRoom.Api.Controllers
 
             return Ok();
         }
-        
+
     }
 }
